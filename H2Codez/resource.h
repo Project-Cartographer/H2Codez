e8f562ecf67e6fe7b00dab0005fb19f7e65129b2
num0005 <double.null@outlook.com>
2017-10-25 23:32:49 +0100

    Add menu item for new instance to sapien and remove mutex.

--- H2Codez/resource.h
+++ H2Codez/resource.h
@@ -1,19 +1,20 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by H2Guerilla.rc
+// Used by H2Sapien.rc
 //
 #define VS_VERSION_INFO                 1
 #define SAPIEN_MENU                     241
 #define GUERILLA_MENU                   1000
 #define ID_EDIT_ADVANCEDSHADERVIEW      40001
 #define ID_FILE_NEWINSTANCE             40002
+#define SAPIEN_FILE_NEWINSTANCE         40005
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        101
-#define _APS_NEXT_COMMAND_VALUE         40004
+#define _APS_NEXT_COMMAND_VALUE         40007
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif

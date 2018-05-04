3815b2b7f6a0bcecbae6e6f5785054901cefbfbf
num0005 <double.null@outlook.com>
2018-05-04 11:42:41 +0100

    Fallback to using original file name for detecting process type.

--- H2Codez/h2codez.cpp
+++ H2Codez/h2codez.cpp
@@ -9,6 +9,7 @@ std::wstring_convert<std::codecvt_utf8<wchar_t>> wstring_to_string;
 
 H2EK H2Toolz::detect_type()
 {
+	// try and detect type based on module name.
 	if (GetModuleHandleW(L"h2Guerilla.exe"))
 	{
 		game.base = GetModuleHandleW(L"h2Guerilla.exe");
@@ -24,6 +25,31 @@ H2EK H2Toolz::detect_type()
 		game.base = GetModuleHandleW(L"h2Tool.exe");
 		return H2EK::H2Tool;
 	}
+	// fallback to checking file information in case the file was renamed.
+	// don't bother error handling
+	wchar_t exe_file_path[_MAX_PATH + 1];
+	assert(0 < (GetModuleFileNameW(NULL, exe_file_path, ARRAYSIZE(exe_file_path))) <= _MAX_PATH);
+
+	DWORD version_info_size = GetFileVersionInfoSizeW(exe_file_path, NULL);
+	assert(version_info_size != 0);
+
+	void *version_info = new BYTE[version_info_size];
+	assert(GetFileVersionInfoW(exe_file_path, NULL, version_info_size, version_info) != 0);
+	wchar_t *orginal_filename;
+	size_t filename_len;
+	// shouldn't be hardcoded but who cares
+	VerQueryValueW(version_info, L"\\StringFileInfo\\040904b0\\OriginalFilename", (LPVOID*)&orginal_filename, &filename_len);
+
+	game.base = GetModuleHandleW(NULL);
+	std::wstring exe_orginal_filename = orginal_filename;
+	delete[] version_info;
+	if (exe_orginal_filename == L"H2Guerilla.exe")
+		return H2EK::H2Guerilla;
+	else if (exe_orginal_filename == L"h2sapien.exe")
+		return H2EK::H2Sapien;
+	else if (exe_orginal_filename == L"h2tool.exe")
+		return H2EK::H2Tool;
+
 	return H2EK::Invalid;
 }
 
@@ -31,6 +57,11 @@ bool H2Toolz::Init()
 {
 	GetCurrentDirectory(sizeof(app_directory), app_directory);
 	game.process_type = detect_type();
+	if (game.process_type == H2EK::Invalid) {
+		MessageBoxA(0, "H2toolz loaded into unsupported process, will now exit!", "ERROR!", MB_OK);
+		return false;
+	}
+
 	DiscordInterface::setAppType(game.process_type);
 	H2CommonPatches::Init();
 
@@ -51,8 +82,7 @@ bool H2Toolz::Init()
 		return true;
 	}
 	case H2EK::Invalid:
-		MessageBoxA(0, "H2toolz loaded into unsupported process, will now exit!", "ERROR!", MB_OK);
-		return false;
+	default:
+		abort(); // should be unreachable
 	}
-	abort(); // should be unreachable
 }

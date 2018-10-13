522e81ea8a42199658ea7d5e029d124ce2d6d11b
num0005 <double.null@outlook.com>
2018-10-13 14:37:02 +0100

    Add more basic types, size checking and conversion functions.

--- H2Codez/Common/BlamBaseTypes.h
+++ H2Codez/Common/BlamBaseTypes.h
@@ -42,6 +42,7 @@ struct colour_rgba
 		blue(_blue)
 	{}
 };
+CHECK_STRUCT_SIZE(colour_rgba, 4 * 4);
 
 struct colour_rgb
 {
@@ -73,17 +74,21 @@ struct colour_rgb
 		return converted;
 	}
 };
+CHECK_STRUCT_SIZE(colour_rgb, 4 * 3);
 
 struct point2d
 {
 	short x;
 	short y;
 };
+CHECK_STRUCT_SIZE(point2d, 2 * 2);
+
 struct real_point2d
 {
 	float x;
 	float y;
 };
+CHECK_STRUCT_SIZE(real_point2d, 4 * 2);
 
 struct real_point3d
 {
@@ -91,37 +96,82 @@ struct real_point3d
 	float y;
 	float z;
 };
+CHECK_STRUCT_SIZE(real_point3d, 4 * 3);
+
+struct angle
+{
+	float rad = 0.0f;
+
+	angle() {};
+
+	angle(float _rad) :
+		rad(_rad)
+	{}
+
+	double as_degree()
+	{
+		return rad * (180.0 / 3.14159265358979323846);
+	}
+
+	double as_rad()
+	{
+		return rad;
+	}
+};
+CHECK_STRUCT_SIZE(angle, sizeof(float));
+CHECK_STRUCT_SIZE(angle, 4);
+
+struct real_euler_angles2d
+{
+	angle yaw;
+	angle pitch;
+};
+CHECK_STRUCT_SIZE(real_euler_angles2d, 4 * 2);
+
+struct real_euler_angles3d
+{
+	angle yaw;
+	angle pitch;
+	angle roll;
+};
+CHECK_STRUCT_SIZE(real_euler_angles3d, 4 * 3);
 
 struct real_vector2d
 {
 	float i;
 	float j;
 };
+CHECK_STRUCT_SIZE(real_vector2d, 4 * 2);
 
 struct real_vector3d
 {
 	float i;
 	float j;
 	float k;
+	real_euler_angles3d get_angle()
+	{
+		real_euler_angles3d angle;
+		angle.yaw = acos(i);
+		angle.pitch = acos(j);
+		angle.roll = acos(k);
+		return angle;
+	}
 };
+CHECK_STRUCT_SIZE(real_vector3d, 4 * 3);
 
 struct real_plane2d
 {
 	real_vector2d normal;
 	float distance;
 };
+CHECK_STRUCT_SIZE(real_plane2d, sizeof(real_vector2d) + 4);
 
 struct real_plane3d
 {
 	real_vector3d normal;
 	float distance;
 };
-
-struct real_euler_angles2d
-{
-	float yaw;
-	float pitch;
-};
+CHECK_STRUCT_SIZE(real_plane3d, sizeof(real_vector3d) + 4);
 
 struct real_bounds
 {

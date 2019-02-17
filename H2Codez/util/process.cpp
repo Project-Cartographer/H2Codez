deedb69d8c4336d32c72d1f786d78356c468a881
num0005 <double.null@outlook.com>
2019-02-17 00:07:28 +0000

    [Process] Add function to fork process.

--- H2Codez/util/process.cpp
+++ H2Codez/util/process.cpp
@@ -26,6 +26,135 @@ bool process::newInstance(std::wstring command_line, HANDLE *new_process_handle,
 	return success;
 }
 
+typedef LONG NTSTATUS, *PNTSTATUS;
+
+typedef struct _CLIENT_ID
+{
+	PVOID UniqueProcess;
+	PVOID UniqueThread;
+}CLIENT_ID, *PCLIENT_ID;
+
+typedef struct _SECTION_IMAGE_INFORMATION
+{
+	PVOID TransferAddress;
+	ULONG ZeroBits;
+	SIZE_T MaximumStackSize;
+	SIZE_T CommittedStackSize;
+	ULONG SubSystemType;
+	union
+	{
+		struct
+		{
+			USHORT SubSystemMinorVersion;
+			USHORT SubSystemMajorVersion;
+		};
+		ULONG SubSystemVersion;
+	};
+	ULONG GpValue;
+	USHORT ImageCharacteristics;
+	USHORT DllCharacteristics;
+	USHORT Machine;
+	BOOLEAN ImageContainsCode;
+	union
+	{
+		UCHAR ImageFlags;
+		struct
+		{
+			UCHAR ComPlusNativeReady : 1;
+			UCHAR ComPlusILOnly : 1;
+			UCHAR ImageDynamicallyRelocated : 1;
+			UCHAR ImageMappedFlat : 1;
+			UCHAR Reserved : 4;
+		};
+	};
+	ULONG LoaderFlags;
+	ULONG ImageFileSize;
+	ULONG CheckSum;
+} SECTION_IMAGE_INFORMATION, *PSECTION_IMAGE_INFORMATION;
+
+
+typedef struct _RTL_USER_PROCESS_INFORMATION
+{
+	ULONG Length;
+	HANDLE Process;
+	HANDLE Thread;
+	CLIENT_ID ClientId;
+	SECTION_IMAGE_INFORMATION ImageInformation;
+} RTL_USER_PROCESS_INFORMATION, *PRTL_USER_PROCESS_INFORMATION;
+
+typedef
+NTSTATUS
+NTAPI
+RtlCloneUserProcess(
+	__in ULONG ProcessFlags,
+	__in_opt PSECURITY_DESCRIPTOR ProcessSecurityDescriptor,
+	__in_opt PSECURITY_DESCRIPTOR ThreadSecurityDescriptor,
+	__in_opt HANDLE DebugPort,
+	__out PRTL_USER_PROCESS_INFORMATION ProcessInformation
+);
+
+#define RTL_CLONE_PROCESS_FLAGS_CREATE_SUSPENDED 0x00000001
+#define RTL_CLONE_PROCESS_FLAGS_INHERIT_HANDLES 0x00000002
+#define RTL_CLONE_PROCESS_FLAGS_NO_SYNCHRONIZE 0x00000004 // don't update synchronization objects
+
+#define RTL_CLONE_PARENT				0
+#define RTL_CLONE_CHILD					297
+
+SYSTEMTIME fake_time;
+VOID WINAPI GetLocalTimeHook(
+	_Out_ LPSYSTEMTIME lpSystemTime
+)
+{
+	if (lpSystemTime)
+	{
+		*lpSystemTime = fake_time;
+	}
+}
+
+bool process::fork(fork_info &info, bool create_suspended)
+{
+	GetLocalTime(&fake_time); // get time so we can 
+	info.pid_other = GetCurrentProcessId();
+	auto ntdll = LOG_CHECK(GetModuleHandleW(L"ntdll"));
+	auto clone_user_process = (RtlCloneUserProcess*)LOG_CHECK(GetProcAddress(ntdll, "RtlCloneUserProcess"));
+
+	if (!clone_user_process)
+		return false;
+
+	RTL_USER_PROCESS_INFORMATION process_info;
+	auto clone_result = clone_user_process(RTL_CLONE_PROCESS_FLAGS_INHERIT_HANDLES | RTL_CLONE_PROCESS_FLAGS_CREATE_SUSPENDED, NULL, NULL, NULL, &process_info);
+
+	if (clone_result == RTL_CLONE_PARENT)
+	{
+		info.pid_other = GetProcessId(process_info.Process);
+		info.handle_other = process_info.Process;
+		info.is_parent = true;
+
+		if (!create_suspended)
+			ResumeThread(process_info.Thread);
+		CloseHandle(process_info.Thread);
+
+		return true;
+	}
+	else if (clone_result == RTL_CLONE_CHILD)
+	{
+		// Replace GetLocalTime to fix crash caused by invalid csr data
+		DetourTransactionBegin();
+		DetourUpdateThread(GetCurrentThread());
+		auto GetLocalTimeOrg = GetLocalTime;
+		DetourAttach(&(PVOID&)GetLocalTimeOrg, GetLocalTimeHook);
+		DetourTransactionCommit();
+
+		// reinit console
+		FreeConsole();
+		AllocConsole();
+		info.is_parent = false;
+		info.handle_other = LOG_CHECK(OpenProcess(PROCESS_QUERY_INFORMATION | READ_CONTROL | SYNCHRONIZE | PROCESS_VM_READ, FALSE, info.pid_other));
+		return true;
+	}
+	return false;
+}
+
 std::wstring process::GetExeDirectoryWide()
 {
 	wchar_t main_exe_dir[MAX_PATH];
--- H2Codez/util/process.h
+++ H2Codez/util/process.h
@@ -5,6 +5,17 @@
 namespace process
 {
 	bool newInstance(std::wstring command_line = L"", HANDLE *new_process_handle = nullptr, const wchar_t *current_directory = nullptr);
+	struct fork_info
+	{
+		bool is_parent;
+		DWORD pid_other;
+		HANDLE handle_other;
+	};
+	/* 
+		Creates a fork using an undocumented windows API, Returns success
+		Doesn't reinitiate the csr connection so a lot of more "complex" functions will crash  
+	*/
+	bool fork(fork_info &info, bool create_suspended = false);
 	std::wstring GetExeDirectoryWide();
 	std::string GetExeDirectoryNarrow();
 	DWORD GetModuleFileNameA(

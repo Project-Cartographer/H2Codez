a370a0264186536eae7b6537a987f4bd5117ab96
num0005 <double.null@outlook.com>
2019-04-13 15:42:41 +0100

    Fix warnings related to assembly.

--- H2Codez/H2Sapien/SapienInterface.cpp
+++ H2Codez/H2Sapien/SapienInterface.cpp
@@ -1,8 +1,9 @@
 #include "SapienInterface.h"
 
-bool SapienInterface::load_structure_bsp(int bsp_block_index, bool unload_old)
+bool SapienInterface::load_structure_bsp(int bsp_block_index, bool _unload_old)
 {
-	static int load_structure_bsp_ptr = 0x4D6830;
+	constexpr static int load_structure_bsp_ptr = 0x4D6830;
+	int unload_old = _unload_old;
 	// char __usercall load_structure_bsp@<al>(int bsp_block_index@<ecx>, char unload_old)
 	__asm {
 		push unload_old

6e54f9ac84cbe01a920248cf4e8929ed17959c7f
num0005 <double.null@outlook.com>
2019-07-15 00:30:50 +0100

    [Util] Add helper code for working with most windows handles.

---
+++ H2Codez/util/SmartHandle.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "stdafx.h"
+
+/*
+	Doesn't fully work for processes because GetCurrentProcess returns INVALID_HANDLE_VALUE
+*/
+class SmartHandle
+{
+	// disable copy
+	SmartHandle(const SmartHandle &) = delete;
+	SmartHandle & operator= (const SmartHandle &) = delete;
+	// allow move
+	SmartHandle(SmartHandle &&) = default;
+	SmartHandle &  operator= (SmartHandle &&) = default;
+
+	SmartHandle(HANDLE handle) :
+		_handle(handle)
+	{};
+
+	/*
+		Attempts to close the handle and returns success
+	*/
+	bool close()
+	{
+		if (!is_valid())
+			return false;
+		bool success = LOG_CHECK(CloseHandle(_handle));
+		if (success)
+			_handle = INVALID_HANDLE_VALUE;
+		return success;
+	}
+
+	/* Is handle valid? */
+	bool is_valid() const { return _handle != INVALID_HANDLE_VALUE && _handle != NULL; }
+
+	HANDLE get() { return  _handle; }
+
+private:
+	HANDLE _handle = INVALID_HANDLE_VALUE;
+
+	~SmartHandle()
+	{
+		close();
+	}
+};
